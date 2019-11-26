254ba8032a1ecee93144fe24d4e66c7ac9b4bf0c
David Strickland <david.robert.strickland@gmail.com>
2019-11-25 23:51:38 -0600

    Change some packages around.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -67,7 +67,12 @@
 (use-package solarized-theme
   :ensure t
   :config
+  (set-face-attribute 'region nil :background "#002b36" :foreground "#fdf6e3")
   (load-theme 'solarized-dark t))
+;; (use-package zenburn-theme
+;;   :ensure t
+;;   :config (load-theme 'zenburn t))
+
 
 (use-package helm
   :ensure t)
@@ -76,20 +81,27 @@
   :ensure t
   :config
   (global-company-mode))
-(use-package ycmd
+
+(use-package flycheck
   :ensure t
   :config
-  (setq ycmd-server-command
-        (list "python" "/Users/david/.vim/bundle/YouCompleteMe/third_party/ycmd/ycmd"))
-  (global-ycmd-mode))
-(use-package company-ycmd
-  :ensure t
-  :config
-  (company-ycmd-setup))
-(use-package flycheck-ycmd
-  :ensure t
-  :config
-  (flycheck-ycmd-setup))
+  (global-flycheck-mode))
+
+;;; (use-package ycmd
+;;;   :ensure t
+;;;   :config
+;;;   (setq ycmd-server-command
+;;;         (list "python" "/Users/david/.vim/bundle/YouCompleteMe/third_party/ycmd/ycmd"))
+;;;   (global-ycmd-mode))
+;;; (use-package company-ycmd
+;;;   :ensure t
+;;;   :config
+;;;   (company-ycmd-setup))
+;;; 
+;;; (use-package flycheck-ycmd
+;;;   :ensure t
+;;;   :config
+;;;   (flycheck-ycmd-setup))
 
 (use-package magit
   :ensure t)
@@ -100,12 +112,23 @@
 (use-package kotlin-mode
   :ensure t)
 
+(use-package elm-mode
+  :ensure t)
+
 (use-package elpy
   :ensure t
   :init
   (elpy-enable)
   :config
   (setq elpy-rpc-python-command "/usr/local/bin/python3"))
+
+(use-package intero
+  :ensure t
+  :config
+  (add-hook 'haskell-mode-hook 'intero-mode))
+
+(use-package markdown-mode
+  :ensure t)
 
 ;; Turn off useless GUI shit
 (tool-bar-mode -1)
@@ -120,8 +143,6 @@
 (when (member "Menlo" (font-family-list)) (set-frame-font "Menlo-13" t t))
 (setq-default vc-follow-symlinks t)
 (setq-default undo-tree-auto-save-history t)
-;; Highlight color
-(set-face-attribute 'region nil :background "#fff" :foreground "#468")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -133,14 +154,15 @@
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (color-theme-solarized evil-magit elpy kotlin-mode magit evil-surround flycheck-ycmd company-ycmd auto-complete flycheck helm solarized-theme use-package evil-visual-mark-mode evil-escape)))
+    (elm-mode flymd markdown-mode zenburn-theme intero color-theme-solarized evil-magit elpy kotlin-mode magit evil-surround flycheck-ycmd company-ycmd auto-complete flycheck helm solarized-theme use-package evil-visual-mark-mode evil-escape)))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Menlo" :foundry "nil" :slant normal :weight normal :height 130 :width normal))))
+ '(region ((t (:foreground "#fdf6e3" :background "#002b36")))))
 
 (provide 'init)
 ;;; init.el ends here
